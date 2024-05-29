a22a8e219850b38245f3c4753460172b41e0c21a
André Cavichiolli <128004877+PetiteSneeze@users.noreply.github.com>
2024-05-29 20:56:06 -0300

    aaa

--- SGFME.Infrastructure.Data/obj/Debug/net8.0/SGFME.Infrastructure.Data.AssemblyInfo.cs
+++ SGFME.Infrastructure.Data/obj/Debug/net8.0/SGFME.Infrastructure.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SGFME.Infrastructure.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd56b1d0607436ece2a9dcaf89dd10545efa0a0c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0ea4f5185c33ace7e305f92411e1efaf9295cc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SGFME.Infrastructure.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SGFME.Infrastructure.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ www/Pages/MedicamentoGerenciamento.cshtml.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace www.Pages
+{
+    public class MedicamentoGerenciamentoModel : PageModel
+    {
+        
+            public string ActivePage { get; private set; }
+
+            public void OnGet()
+            {
+                ActivePage = "MedicamentoGerenciamento";
+            }
+
+        
+    }
+}
